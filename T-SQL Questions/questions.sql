9ff6dc67393aef01e7e9873dd586a64d7bd82373
Phumza Nkosi <88560590+Comfort10@users.noreply.github.com>
2026-07-13 21:30:11 +0200

    Update questions.sql

--- T-SQL Questions/questions.sql
+++ T-SQL Questions/questions.sql
@@ -1,3 +1,35 @@
 -- Q1
 -- Return orders placed in June 2021
 -- Tables involved: TSQLV6 database, Sales.Orders table
+
+
+
+-- Q2 
+-- Return orders placed on the day before the last day of the month
+-- Tables involved: Sales.Orders table
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
